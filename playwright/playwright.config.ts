6cdb741b49e82aa55a9cab2c680c08c09e74c680
jgg <jordigirosguerrero@gmail.com>
2025-02-24 12:05:46 +0100

    Slightly increase of tiemout for low performance computers

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
-    timeout: 10000,
+    timeout: 15000,
     use: {
         headless: true,
         viewport: { width: 1920, height: 1080 },
